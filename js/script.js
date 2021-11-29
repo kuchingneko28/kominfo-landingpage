e1d60751241acc666889efff6dc44f16cf9ab630
kuchingneko28 <kuchingneko28@gmail.com>
2021-11-30 02:23:49 +0800

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const navbar = document.querySelector(".navbar");
 const burger = document.querySelector(".burger");
 const navMenu = document.querySelector(".nav-link");
 
+// Check click
 burger.addEventListener("click", () => {
   burger.classList.toggle("toggle");
   navMenu.classList.toggle("nav-active");
